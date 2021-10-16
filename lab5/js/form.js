1485fe423e6b76b2f78341bf39e61eef30337ae2
Te1man1 <90602442+Te1man1@users.noreply.github.com>
2021-10-16 23:45:46 +0300

    Add files via upload

---
+++ lab5/js/form.js
@@ -0,0 +1,9 @@
+
+function click1() {
+  let f1 = document.getElementsByName("cena");
+  let f2 = document.getElementsByName("kolichestvo");
+  let r = document.getElementById("result");
+  r.innerHTML = f1[0].value * f2[0].value;
+}
+
+  
